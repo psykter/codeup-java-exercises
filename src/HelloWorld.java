90592691c6c60ab5c2998f1ed2e0f57f290dfc58
Rick Diaz <richard.david.diaz@gmail.com>
2023-12-12 14:36:10 -0600

    First commit in Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,34 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("This is an example of a statement");
+        System.out.println("Notice that it ends with a semicolon");
+        System.out.println("When we create statements: they MUST end with a semicolon.");
+        System.out.println("Or Java will get mad at us.");
+
+        /*
+         * This is a mulit-line comment.
+         * It can go on for multiple lines.
+         * It is useful for commenting out large sections of code.
+         * Ain't that neat?
+         */
+
+        byte smallNum = 19;
+        System.out.println(smallNum);
+        smallNum = 126;
+        smallNum++;
+        System.out.println(smallNum);
+
+        char firstLetterOfName = 'J';
+
+        String name = "Jesse"; // String is a class, not a primitive type, double quotes (objects)
+        String quote = "\"This is a quote\""; // escape character
+        System.out.println(quote);
+        System.out.println(name);
+
+        int numDefault;
+        System.out.println(
+            "This is the default value of an int: " + numDefault; // default value is 0
+
+        final String EMPORER_OF_IMUGI = "Rick \"The Ruler\" Sanchez"; // convention for constants is all caps
     }
 }
